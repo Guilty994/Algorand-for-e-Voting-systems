98a1a74c06e0ab62c89f39b533387d316932e333
Domenico Liguori (laptop) <mondaynight1@live.it>
2022-05-12 14:53:28 +0200

    laptop commit

---
+++ src/ENV.js
@@ -0,0 +1,9 @@
+export const ENV = {
+    algodToken : {"X-API-Key": "cVUq9NZ8jb23hAlrL3GvBGzx67my0aA4YkjLUiZ7"},
+    baseServer : 'https://testnet-algorand.api.purestake.io/ps2/',
+    port : '',
+    elecAuthMnemonic : "",
+    regAuthMnemonic : "",
+    voterMnemonic : "",
+    candidateMnemonic : "",
+    }
